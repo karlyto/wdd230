ba45a4a42888c5c7be1f527c9934745f4d2acac2
karlyto <mic21016@byui.edu>
2022-05-17 01:24:19 +0300

    done

--- date.js
+++ date.js
@@ -1,1 +1,5 @@
 document.querySelector('#lastUpdate').innerHTML = `Date Modified: ${document.lastModified}`;
+
+let currentDate = new Date();
+let Year = currentDate.getFullYear();
+document.getElementById("copyright").textContent = Year;
