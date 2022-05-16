2fa14733ebe5e2df49587e0b11265d0c419c4f18
karlyto <mic21016@byui.edu>
2022-05-17 02:01:51 +0300

    Done

--- lesson2/js/getdate.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#lastUpdate').innerHTML = `Date Modified: ${document.lastModified}`;
-
-let currentDate = new Date();
-let Year = currentDate.getFullYear();
-document.getElementById("copyright").textContent = Year;
