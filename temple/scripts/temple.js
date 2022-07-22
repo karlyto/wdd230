0bd863f3b31ef42ffc2397a0ffa19479ff3659ae
karlyto <mic21016@byui.edu>
2022-07-22 05:38:24 +0300

    ok

---
+++ temple/scripts/temple.js
@@ -0,0 +1,57 @@
+
+const requestURL = 'json/data.json';
+const likeCounter = document.createElement("span")
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const companies = jsonObject['companies'];
+    companies.forEach(displaycompanies);
+  });
+
+  
+  function displaycompanies(company) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let image = document.createElement('img');
+    let phone = document.createElement('p');
+    let address = document.createElement('p');
+    let website = document.createElement('p');
+    let membership = document.createElement('p');
+    let email = document.createElement('p');
+  
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    h2.textContent = `${company.name}`;
+
+    image.setAttribute('src', company.image);
+    image.setAttribute('alt', `image of ${company.name}`);
+    image.setAttribute('loading', 'lazy');
+    email.textContent = `${company.email}`;
+    phone.textContent = `${company.phone}`;
+    address.textContent = `${company.address}`;
+    membership.textContent = `${company.membership}`;
+    website.innerHTML = `<a href="${company.url}" target="_blank">Additional Information</a>`;
+    
+
+    
+  
+    // Add/append the section(card) with the h2 element
+    card.appendChild(h2)
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(email);
+    card.appendChild(website);
+    card.appendChild(membership);
+    card.appendChild(image);
+
+
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+  }
+
+
---
+++ temple/scripts/windchill.js
@@ -0,0 +1,63 @@
+const temperature = document.querySelector('#temperature');
+const wind = document.querySelector('#wind');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#condition');
+let chill = document.querySelector('#chill');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=8ff9999d552995216615d400071558b8&units=metric';
+
+// weather data------------------------------------------------------------------------------//
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    temperature.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = capitalize(desc);
+    wind.innerHTML = weatherData.wind.speed;
+  }
+
+  const capitalize = (str) => {
+    let strArr = str.split(" ");
+    return strArr.map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+}
+
+
+
+let xtemp = parseFloat(temperature.textContent);
+let y = parseFloat(wind.textContent);
+
+xtemp = (xtemp*(9/5)) +32;
+y = y/1.60934;
+
+console.log(xtemp);
+console.log(y);
+let result = 0;
+let resulttext = ""; 
+if ((xtemp <= 50) && (y > 3) ){
+    result =  35.74 + (0.6215*xtemp) - (35.75*(y**0.16)) + (0.4275*xtemp*(y**0.16)) ;
+    chill.innerHTML = `${result.toFixed(1)} F°`;
+}else{
+    resulttext = "N/A"
+    chill.innerHTML = resulttext;
+}
+
+// weather data------------------------------------------------------------------------------//
+
